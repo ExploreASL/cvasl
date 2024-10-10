fcdce047bd500520c45476cba086223d8c1b14b4
Saba Amiri <sabaamiri@ID14492s-MacBook-Pro.local>
2024-10-10 06:42:58 +0200

    remaining commits

--- cvasl/_version.py
+++ cvasl/_version.py
@@ -12,5 +12,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.0.4.dev41+g2ffb7b8'
-__version_tuple__ = version_tuple = (0, 0, 4, 'dev41', 'g2ffb7b8')
+__version__ = version = '0.0.4.dev42+gbe937f6.d20241010'
+__version_tuple__ = version_tuple = (0, 0, 4, 'dev42', 'gbe937f6.d20241010')
