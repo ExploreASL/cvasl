8e04b3a5784d25eef1ff621fb0683d69ab7523aa
Saba Amiri <s.amiri@esciencecenter.nl>
2025-02-03 10:07:54 +0100

    repo cleanup, needs a final check

--- cvasl/runharmonize.py
+++ cvasl/runharmonize.py
@@ -1,11 +1,10 @@
 import pandas as pd
 import sys
-sys.path.insert(0, '../../')
-sys.path.insert(0, '../')
-sys.path.insert(0, '../../../')
-sys.path.insert(0, '../../../../')
 import numpy as np
-from cvasl.mriharmonize import *
+from cvasl.dataset import *
+from cvasl.harmonizers import *
+from cvasl.prediction import *
+
 from sklearn.ensemble import ExtraTreesRegressor
 
 import warnings
@@ -34,7 +33,7 @@ datasets = [edis, helius, sabre, topmri, insight46]
 
 datasets = encode_cat_features(datasets,features_to_map)
 
-method = 'neuroharmonize'
+method = 'autocombat' 
 
 
 if method == 'neuroharmonize':
@@ -43,7 +42,7 @@ if method == 'neuroharmonize':
     covariates = ['age', 'sex',  'icv', 'site']
     site_indicator = 'site'
     #ATTENTION: providing the smoothing term, e.g. ['age'] leads to longer running time and different results
-    harmonizer = HarmNeuroHarmonize( features_to_harmonize = features_to_harmonize, covariates = covariates, smooth_terms = [], site_indicator=site_indicator, empirical_bayes = True)
+    harmonizer =  NeuroHarmonize( features_to_harmonize = features_to_harmonize, covariates = covariates, smooth_terms = [], site_indicator=site_indicator, empirical_bayes = True)
     harmonized_data = harmonizer.harmonize(datasets)
     
     
@@ -55,7 +54,7 @@ elif method == 'covbat':
     features_to_harmonize = [patient_identifier, site_indicator, 'age', 'sex', 'site', 'aca_b_cov', 'mca_b_cov', 'pca_b_cov', 'totalgm_b_cov', 'aca_b_cbf', 'mca_b_cbf', 'pca_b_cbf', 'totalgm_b_cbf']
     numerical_covariates = ['age']
     covariates = ['age', 'sex']    
-    harmonizer = HarmCovbat(features_to_harmonize=features_to_harmonize,covariates=covariates,site_indicator=site_indicator,patient_identifier=patient_identifier,numerical_covariates=numerical_covariates)
+    harmonizer =  Covbat(features_to_harmonize=features_to_harmonize,covariates=covariates,site_indicator=site_indicator,patient_identifier=patient_identifier,numerical_covariates=numerical_covariates)
     harmonized_data = harmonizer.harmonize(datasets)
 
 elif method == 'neurocombat':
@@ -65,23 +64,32 @@ elif method == 'neurocombat':
     discrete_covariates= ['sex']
     continuous_covariates=  ['age']
     site_indicator = 'site'
-    harmonizer = HarmNeuroCombat(features_to_harmonize = features_to_harmonize,discrete_covariates= discrete_covariates,continuous_covariates=  continuous_covariates,site_indicator = site_indicator,patient_identifier=patient_identifier)
+    harmonizer =  NeuroCombat(features_to_harmonize = features_to_harmonize,discrete_covariates= discrete_covariates,continuous_covariates=  continuous_covariates,site_indicator = site_indicator,patient_identifier=patient_identifier)
     harmonized_data = harmonizer.harmonize(datasets)
 
 elif method == 'nestedcombat':
-    
-    features_to_harmonize = [
-        'age', 'sex', 'ACA_B_CoV', 'MCA_B_CoV', 'PCA_B_CoV', 'TotalGM_B_CoV',
-        'ACA_B_CBF', 'MCA_B_CBF', 'PCA_B_CBF', 'TotalGM_B_CBF',
-    ]
-    site_indicator = ['site']
-    #ATTENTION: When I add readout to the discrete covariates, the code throws an error related to calculating the singular matrix, apprently messes up svd operations somehow
-    discrete_covariates = ['sex','labelling']
-    continuous_covariates = ['age','ld','pld']
-    to_be_harmonized_or_covar  = [x.lower() for x in features_to_harmonize ]
-    harmonizer = HarmNestedComBat(features_to_harmonize= features_to_harmonize,  site_indicator = site_indicator, discrete_covariates = discrete_covariates, continuous_covariates = continuous_covariates, intermediate_results_path = '.', return_extended = False, patient_identifier=patient_identifier)
-    harmonized_data = harmonizer.harmonize(datasets)
 
+    features_to_harmonize = [
+        'ACA_B_CoV', 'MCA_B_CoV', 'PCA_B_CoV', 'TotalGM_B_CoV',
+        'ACA_B_CBF', 'MCA_B_CBF', 'PCA_B_CBF', 'TotalGM_B_CBF'
+    ]
+    batch_list_harmonisations = ['readout', 'ld', 'pld']
+    site_indicator = ['site']
+    discrete_covariates = ['sex']
+    continuous_covariates = ['age']
+
+    harmonizer =  NestedComBat(
+        features_to_harmonize=features_to_harmonize,
+        batch_list_harmonisations=batch_list_harmonisations,
+        site_indicator=site_indicator,
+        discrete_covariates=discrete_covariates,
+        continuous_covariates=continuous_covariates,
+        intermediate_results_path='.',
+        return_extended=False,
+        use_gmm=False
+    )
+
+    harmonized_data = harmonizer.harmonize(datasets)
 elif method == 'comscanneuroharmonize':
     
     features_to_harmonize = ['aca_b_cov', 'mca_b_cov', 'pca_b_cov', 'totalgm_b_cov', 
@@ -89,19 +97,23 @@ elif method == 'comscanneuroharmonize':
     discrete_covariates = ['sex']
     continuous_covariates = ['decade']
     site_indicator = 'site'
-    harmonizer = HarmComscanNeuroCombat(features_to_harmonize=features_to_harmonize,discrete_covariates=discrete_covariates,continuous_covariates=continuous_covariates,site_indicator=site_indicator) 
+    harmonizer =  ComscanNeuroCombat(features_to_harmonize=features_to_harmonize,discrete_covariates=discrete_covariates,continuous_covariates=continuous_covariates,site_indicator=site_indicator) 
     harmonized_data = harmonizer.harmonize(datasets)
+
+
 
 elif method == 'autocombat':
     
     features_to_harmonize = ['aca_b_cov', 'mca_b_cov', 'pca_b_cov', 'totalgm_b_cov', 
                              'aca_b_cbf', 'mca_b_cbf', 'pca_b_cbf', 'totalgm_b_cbf',]
+    data_subset = ['aca_b_cov', 'mca_b_cov', 'pca_b_cov', 'totalgm_b_cov', 
+                             'aca_b_cbf', 'mca_b_cbf', 'pca_b_cbf', 'totalgm_b_cbf','site','readout','labelling','pld','ld','sex','age']
     discrete_covariates = ['sex']
-    continuous_covariates = ['decade']
-    sites=['site','ld','pld','readout','labelling']
-    discrete_cluster_features = ['readout','labelling']
-    continuous_cluster_features = ['ld','pld'] 
-    harmonizer = HarmAutoCombat(features_to_harmonize = features_to_harmonize, site_indicator=sites, discrete_covariates = discrete_covariates, continuous_covariates = continuous_covariates, continuous_cluster_features=continuous_cluster_features, discrete_cluster_features=discrete_cluster_features)
+    continuous_covariates = ['age']
+    sites=['site','readout','pld','ld']
+    discrete_cluster_features = ['site','readout']
+    continuous_cluster_features = ['pld','ld'] 
+    harmonizer =  AutoCombat(data_subset = data_subset, features_to_harmonize = features_to_harmonize, site_indicator=sites, discrete_covariates = discrete_covariates, continuous_covariates = continuous_covariates, continuous_cluster_features=continuous_cluster_features, discrete_cluster_features=discrete_cluster_features)
     harmonized_data = harmonizer.harmonize(datasets)
 
 elif method == 'relief':
@@ -110,7 +122,7 @@ elif method == 'relief':
                              'aca_b_cbf', 'mca_b_cbf', 'pca_b_cbf', 'totalgm_b_cbf']
     covars = ['sex','age']
     
-    harmonizer = HarmRELIEF(features_to_harmonize=features_to_harmonize,covariates=covars,patient_identifier=patient_identifier) 
+    harmonizer =  RELIEF(features_to_harmonize=features_to_harmonize,covariates=covars,patient_identifier=patient_identifier) 
     harmonized_data = harmonizer.harmonize([topmri, helius, edis,  sabre,  insight46])
 
 elif method == 'combat++':
@@ -122,13 +134,14 @@ elif method == 'combat++':
     discrete_covariates_to_remove = ['labelling']
     continuous_covariates_to_remove = ['ld']
     sites='site'
-    harmonizer = HarmCombatPlusPlus(features_to_harmonize = features_to_harmonize, site_indicator=sites, discrete_covariates = discrete_covariates, continuous_covariates = continuous_covariates, discrete_covariates_to_remove = discrete_covariates_to_remove, continuous_covariates_to_remove = continuous_covariates_to_remove) 
+    harmonizer =  CombatPlusPlus(features_to_harmonize = features_to_harmonize, site_indicator=sites, discrete_covariates = discrete_covariates, continuous_covariates = continuous_covariates, discrete_covariates_to_remove = discrete_covariates_to_remove, continuous_covariates_to_remove = continuous_covariates_to_remove) 
     harmonized_data = harmonizer.harmonize(datasets)
 
 print(harmonized_data[1].data.tail())
+
 [_d.prepare_for_export() for _d in datasets]
 print(harmonized_data[1].data.tail())
-
+topmri.data.to_csv(Edis_path.replace('input',f'input_topmri'),index=False)
 
 harmonized_data[0].data.to_csv(Edis_path.replace('input',f'output_{method}'),index=False)
 harmonized_data[1].data.to_csv(helius_path.replace('input',f'output_{method}'),index=False)
